fcdeb107f8d024ab73f28b2d5c8a3796822fd3de
Nemeth Angela <nemethangela75@gmail.com>
2024-01-27 10:30:00 +0100

    init

---
+++ Nobel_CSharp/NobelDij.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Nobel_CSharp
+{
+    internal class NobelDij
+    {
+        public int Year { get; set; }
+        public string Tipus { get; set; }
+        public string Keresztnev { get; set; }  
+        public string Vezeteknev { get; set; }
+
+        public override string ToString()
+        {
+            return $"Év: {Year}, Típus: {Tipus}, Név: {Keresztnev} {Vezeteknev}";
+        }
+    }
+}
